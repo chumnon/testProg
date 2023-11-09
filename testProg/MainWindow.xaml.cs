e0c4b4e77ba8aafe101ac0bd499288cc08618888
chumnon <143432643+chumnon@users.noreply.github.com>
2023-11-09 08:38:30 -0500

    Update MainWindow.xaml.cs

--- testProg/MainWindow.xaml.cs
+++ testProg/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace testProg
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            myButton.Content = "pogger";
         }
     }
 }
